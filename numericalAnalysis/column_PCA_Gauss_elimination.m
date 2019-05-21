2a18732de4127d12a9606c378f5ec404fe8db82d
chenfeng123456 <1294159086@qq.com>
2019-05-21 22:05:22 +0800

    modified numericalAnalysis/column_PCA_Gauss_elimination.m

--- numericalAnalysis/column_PCA_Gauss_elimination.m
+++ numericalAnalysis/column_PCA_Gauss_elimination.m
@@ -1,14 +1,14 @@
-function X = column_PCA_Gauss_elimination(A, b, n)
+function X = column_PCA_Gauss_elimination(A, b)
 % A --> coefficients matrix
 % b --> right-hand member
-% n --> orders
 
+n = length(b);
 for k = 1 : n - 1
-   alpha = A(k, k);
+   alpha = abs(A(k, k));
    i_k = k;
    for i = k+1 : n
-       if alpha < A(k, i)
-           alpha = A(k, i);
+       if alpha < abs(A(i, k))
+           alpha = abs(A(i, k));
            i_k = i;
        end
    end
@@ -20,23 +20,25 @@ for k = 1 : n - 1
        temp = A(i_k, j);
        A(i_k, j) = A(k, j);
        A(k, j) = temp;
-       temp = b(i_k);
-       b(i_k) = b(k);
-       b(k) = temp;
    end
+   temp = b(i_k);
+   b(i_k) = b(k);
+   b(k) = temp;
+   
    m = ones(1, n);
    for i = k+1 : n
        m(i) = A(i, k) / A(k, k);
-       A(i, k) = 0;
    end
    for i = k+1 : n
-       for j = k+1 : n
-           A(i, j) = A(i, j) - m(i) * A(k, j)
+       for j = k : n
+           A(i, j) = A(i, j) - m(i) * A(k, j);
        end
        b(i) = b(i) - m(i) * b(k);
    end
 end
 
+
+b(n) = b(n) / A(n, n);
 for i = n-1 : -1 : 1
     sum = 0;
     for j = i+1 : n
@@ -44,4 +46,4 @@ for i = n-1 : -1 : 1
     end
     b(i) = (b(i) - sum) / A(i, i);
 end
-X = b
+X = b;
